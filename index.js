f74c053fe828726a2e9bdc65656a643e094cc584
Erick García <erickDevUp@gmail.com>
2023-03-11 16:06:48 +0100

    fix

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const { default: axios } = require("axios");
 const Parser = require("rss-parser");
 let parser = new Parser();
 
+
 const { Telegraf } = require("telegraf");
 
 const { message } = require("telegraf/filters");
 
 const bot = new Telegraf("6092909492:AAHBFwlrfjTDsG8dezYnWZM8P3psnZlli48");
 
+app.use(async()=>await bot.createWebhook({ domain: "https://api.telegram.org" }));
 
 bot.start((ctx) => ctx.reply("⭐Welcome  " + ctx.from.first_name+"\n\n🪐 Hi there! I can help you with that. Here is a brief description for a Telegram bot that shows you astronomy photos and news every day:\n\nThis bot offers a daily dose of astronomy by sending you a breathtaking photo of space along with some interesting news about the field. Each day, you will receive a new photo and news article to keep you informed and inspired. The bot is perfect for anyone interested in astronomy, space exploration and the wonders of the universe. Never miss out on the latest developments in astronomy again - let this bot keep you up to date and inspired every day!\n\n commands : \n\n/pic: get the Astronomy Picture of the Day\n\n /news: get the most important news of the day "));
 bot.on(message("sticker"), (ctx) => ctx.reply("🪐"));
